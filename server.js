32b13110346f89c86ed74398a3e88e2fa153e19e
Glitch (codersx-express-lesson1) <none>
2020-06-26 12:17:09 +0000

    😼🔨 Checkpoint ./server.js:18003481/1044

--- server.js
+++ server.js
@@ -3,12 +3,12 @@
 
 // we've started you off with Express (https://expressjs.com/)
 // but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require('express');
+const express = require("express");
 const app = express();
 
 // https://expressjs.com/en/starter/basic-routing.html
-app.get('/', (request, response) => {
-  response.send('I love CodersX');
+app.get("/", (request, response) => {
+  response.send("I love CodersX");
 });
 
 // listen for requests :)
